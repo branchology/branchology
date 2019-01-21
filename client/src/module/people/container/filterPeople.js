85697a48574a7a7b8daa1c60cd91c9e03b60be90
Kristopher Wilson <kristopherwilson@gmail.com>
2019-01-21 14:45:17 -0500

    Add default people sorting

--- client/src/module/people/container/filterPeople.js
+++ client/src/module/people/container/filterPeople.js
@@ -2,6 +2,11 @@ import gql from 'graphql-tag';
 import React from 'react';
 import { Query } from 'module/common';
 
+const defaultSorting = [
+  { field: 'surname', order: 'ASC' },
+  { field: 'given', order: 'ASC' },
+];
+
 const filterPeople = gql`
   query filterPeople($filter: PersonFilter) {
     people(filter: $filter, paging: { perPage: 25 }) {
@@ -24,7 +29,10 @@ const filterPeople = gql`
 `;
 
 export default WrappedComponent => ({ search: nameContains, ...props }) => (
-  <Query query={filterPeople} variables={{ filter: { nameContains } }}>
+  <Query
+    query={filterPeople}
+    variables={{ filter: { nameContains }, sorting: defaultSorting }}
+  >
     {({ loading, data }) => {
       if (loading) return null;
       return <WrappedComponent data={data} {...props} />;
